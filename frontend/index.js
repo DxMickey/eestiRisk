53c268e8eb3def6f5048a1cf04a842995aba5e10
DxMickey <jannosepajoe@gmail.com>
2021-05-05 18:47:33 +0300

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 
-const socket = io('https://safe-taiga-41855.herokuapp.com/');
+const socket = io('https://glacial-fortress-00623.herokuapp.com/');
 
 socket.on('init',handleInit);
 socket.on('gameState',handleGameState);
